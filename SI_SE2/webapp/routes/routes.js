4236170a52a71ee0b6794a432337cc2f74fadcc6
Nunobpinto <nunobernardopinto@gmail.com>
2017-11-09 14:41:46 +0000

    begun callback

--- SI_SE2/webapp/routes/routes.js
+++ SI_SE2/webapp/routes/routes.js
@@ -3,28 +3,44 @@ const router = express.Router()
 const authService = require('../data/service/authService')
 const global = require('../data/global')
 const query = require('querystring')
-
+const request = require('request')
 
 router.get('/', function(req, res) {
 	res.render('home')
 })
 
-router.get('/login',function (req,res,next) {
+router.get('/google', function(req, res) {
 	res.statusCode = 302
 	let queryString = query.stringify({
-		scope :'openid%20email',
-		redirect_uri : global.Google_Redirect_URI ,
-		response_type : 'code',
-		client_id : global.Google_ClientID,
-		prompt : 'consent'
+		scope: 'openid email',
+		redirect_uri: global.Google_Redirect_URI,
+		response_type: 'code',
+		client_id: global.Google_ClientID,
+		prompt: 'consent'
 	})
-	res.set({
-		Location : 'https://accounts.google.com/o/oauth2/v2/auth?' + queryString
-	})
+	res.set({Location: 'https://accounts.google.com/o/oauth2/v2/auth?' + queryString})
+	res.end()
 })
 
-router.get('/login/callback',function (req,res,next) {
-	res.render('home')
+router.get('/google/callback', function(req, res) {
+	const code = req.query['code']
+	console.log('Authorization code is = ' + code)
+
+	const params = {
+		code: code,
+		client_id: global.Google_ClientID,
+		client_secret: global.Google_ClientSecret,
+		redirect_uri: global.Google_Redirect_URI,
+		grant_type: 'authorization_code'
+	}
+
+	request.post(
+		'https://www.googleapis.com/oauth2/v4/token',
+		{json: true, form: params},
+		function(err, resp, data) {
+			//TODO: do stuff
+		}
+	)
 })
 
 module.exports = router
