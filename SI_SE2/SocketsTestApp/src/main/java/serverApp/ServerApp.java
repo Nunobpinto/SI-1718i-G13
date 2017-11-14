2d1624dc517763a26dc1466471ee864668311c67
brunofilipe <bruno.filipe1996@hotmail.com>
2017-11-14 16:31:04 +0000

    starting exercise 5

--- SI_SE2/SocketsTestApp/src/main/java/serverApp/ServerApp.java
+++ SI_SE2/SocketsTestApp/src/main/java/serverApp/ServerApp.java
@@ -1,4 +1,32 @@
 package serverApp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
+
+    public static void main(String[] args) {
+        int portNumber = Integer.parseInt(args[0]);
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out =
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
 }
